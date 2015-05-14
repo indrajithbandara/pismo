40b9714ccb84fb8a4ac17c4daacd6a1e1f503037
Tran Minh Tuan <tuantranf@gmail.com>
2015-05-14 23:55:41 +0900

    Added description match pattern

--- lib/pismo/description_matches.rb
+++ lib/pismo/description_matches.rb
@@ -1,7 +1,9 @@
 DESCRIPTION_MATCHES = [
+  ['meta[@property="og:description"]', lambda { |el| el.attr('content') }],
   ['meta[@name="description"]', lambda { |el| el.attr('content') }],
   ['meta[@name="Description"]', lambda { |el| el.attr('content') }],
   ['meta[@name="DESCRIPTION"]', lambda { |el| el.attr('content') }],
   'rdf:Description[@name="dc:description"]',
-  '.description'
+  '.description',
+  '@itemprop="description"'
 ]
